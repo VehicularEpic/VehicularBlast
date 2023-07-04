0d3feb35eccfd4c153e1c6ccbb728c2a1f693734
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2023-07-04 15:52:03 -0300

    Enable face culling & depth testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "gui/Window.hpp"
 
+#include <glad/gl.h>
+
 int main(int argc, char const *argv[]) {
     veb::Window window("Vehicular Blast");
 
-    while (window.IsActive())
+    glEnable(GL_CULL_FACE);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    while (window.IsActive()) {
+        window.Clear();
         window.Update();
+    }
 
     return 0;
 }
