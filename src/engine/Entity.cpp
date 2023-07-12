a508bbb0dee781c96f836a96949f8afc2ae0fe0c
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2023-07-12 11:39:22 -0300

    Basic Entity class implementation

---
+++ src/engine/Entity.cpp
@@ -0,0 +1,21 @@
+#include "Entity.hpp"
+
+#include <glm/vec3.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/ext/matrix_transform.hpp>
+
+namespace veb {
+
+glm::mat4 Entity::GetModelMatrix() const {
+    glm::mat4 matrix = glm::identity<glm::mat4>();
+    matrix = glm::translate(matrix, glm::vec3(x, -z, -y));
+
+    // https://en.wikipedia.org/wiki/Aircraft_principal_axes
+    matrix = matrix * glm::toMat4(glm::quat(glm::cos(pitch / 2.f), glm::sin(pitch / 2.f), 0.f, 0.f));
+    matrix = matrix * glm::toMat4(glm::quat(glm::cos(yaw / 2.f), 0.f, 0.f, glm::sin(yaw / 2.f)));
+    matrix = matrix * glm::toMat4(glm::quat(glm::cos(roll / 2.f), 0.f, -glm::sin(roll / 2.f), 0.f));
+
+    return matrix;
+}
+
+} // namespace veb
---
+++ src/engine/Entity.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "../gfx/Mesh.hpp"
+
+#include <glm/mat4x4.hpp>
+
+namespace veb {
+
+class Entity {
+private:
+    const Mesh &mesh;
+
+    float x = 0.f;
+    float y = 0.f;
+    float z = 0.f;
+
+    float pitch = 0.f;
+    float yaw = 0.f;
+    float roll = 0.f;
+
+public:
+    Entity(const Mesh &mesh) : mesh(mesh) {}
+    ~Entity() = default;
+
+    void Render() const {
+        mesh.Render();
+    }
+
+    void SetPosition(float px, float py, float pz) {
+        x = px;
+        y = py;
+        z = pz;
+    }
+
+    void SetX(float px) {
+        x = px;
+    }
+
+    void SetY(float py) {
+        y = py;
+    }
+
+    void SetZ(float pz) {
+        z = pz;
+    }
+
+    void SetRotation(float x, float y, float z) {
+        pitch = x;
+        yaw = y;
+        roll = z;
+    }
+
+    void SetPitch(float x) {
+        pitch = x;
+    }
+
+    void SetYaw(float y) {
+        yaw = y;
+    }
+
+    void SetRoll(float z) {
+        roll = z;
+    }
+
+    glm::mat4 GetModelMatrix() const;
+};
+
+} // namespace veb
