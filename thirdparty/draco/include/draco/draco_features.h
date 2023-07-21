fa5c90687d9e08a70a0b977f05b1354ab8456420
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2023-07-21 11:48:12 -0300

    Minimal draco build

---
+++ thirdparty/draco/include/draco/draco_features.h
@@ -0,0 +1,17 @@
+#ifndef DRACO_FEATURES_H_
+#define DRACO_FEATURES_H_
+
+#define DRACO_MESH_COMPRESSION_SUPPORTED
+#define DRACO_NORMAL_ENCODING_SUPPORTED
+#define DRACO_STANDARD_EDGEBREAKER_SUPPORTED
+
+#pragma warning(disable : 4018)
+#pragma warning(disable : 4146)
+#pragma warning(disable : 4244)
+#pragma warning(disable : 4267)
+#pragma warning(disable : 4305)
+#pragma warning(disable : 4661)
+#pragma warning(disable : 4800)
+#pragma warning(disable : 4804)
+
+#endif // DRACO_FEATURES_H_
